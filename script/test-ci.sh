07a41fb0d0bd5f09bb79eaa6e43f575fccf18f99
Lukasz Bandzarewicz <lucassus@gmail.com>
2013-09-24 12:39:47 +0200

    feat(server): wait till the backend server is fully operational

--- script/test-ci.sh
+++ script/test-ci.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-coffee ./server/index.coffee &
-PID=$!
+SERVER_PORT=5000
+
+# start the backend server and wait till it's fully operational
+PORT="$SERVER_PORT" coffee ./server/index.coffee &
+while ! nc -vz localhost "$SERVER_PORT" &> /dev/null; do
+  echo "Waiting for the backend server..."
+  sleep 0.250
+done
+
+echo "Backend server is fully operational!"
+
+# grab the PID
+SERVER_PID=$!
+
+# run all specs
 grunt test:ci
-kill -9 "$PID"
+
+# kill the backend server
+kill -9 "$SERVER_PID"
